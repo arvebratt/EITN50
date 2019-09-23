b1ef1a0e511e7089566d67ef0142daf458aa40c4
Filip Åhfelt <47358668+filahf@users.noreply.github.com>
2019-09-23 11:54:21 +0200

    Basic server and client example

--- OBJsec/client.py
+++ OBJsec/client.py
@@ -1,1 +1,34 @@
-print("hejhej")
+#https://pythontic.com/modules/socket/udp-client-server-example
+import socket
+
+
+
+msgFromClient       = "Hello UDP Server"
+
+bytesToSend         = str.encode(msgFromClient)
+
+serverAddressPort   = ("127.0.0.1", 20001)
+
+bufferSize          = 1024
+
+
+
+# Create a UDP socket at client side
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+
+
+# Send to server using created UDP socket
+
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+
+
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
