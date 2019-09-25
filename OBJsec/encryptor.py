29dc6e4c07ab726fa2cf3d220b6d4695c0f008b1
Hirschan <45165564+Hirschan@users.noreply.github.com>
2019-09-25 14:42:25 +0200

    Update encryptor.py

--- OBJsec/encryptor.py
+++ OBJsec/encryptor.py
@@ -3,21 +3,17 @@ from cryptography.fernet import Fernet
 
 key = Fernet.generate_key() #creates a random key everytime being called upon
 
-#Save the key so you can encrypt/decrypt with the same key
-file = open('key.key', 'wb')
-file.write(key)  #the key is type bytes, bytes object, therefore 'b'
-file.close()
 print("\nOur key is: " + str(key))
 
 #Encode the message
 message = "my deep dark secret"
 encoded = message.encode()
-print("\nMessage to encrypt is: " + str(message))
+print("\n1. Message to encrypt is: " + str(message))
 
 #Encrypt the message
 f = Fernet(key)
 encrypted = f.encrypt(encoded)
-print("\nMessage is now encrypted into: " + str(encrypted))
+print("\n2. Message is now encrypted into: " + str(encrypted))
 
 #decrypt
 f2 = Fernet(key)
@@ -25,4 +21,4 @@ decrypted = decrypted = f2.decrypt(encrypted)
 
 #Decode the message
 original_message = decrypted.decode()
-print ("\nThis is the message decoded: " + str(original_message))
+print ("\n3. This is the message decoded and decrypted: " + str(original_message))
